45c9250d2b9ca4f2abbc766e29170b025475838a
Krisjan <thefalseprophet384@gmail.com>
2024-08-12 21:45:23 -0400

    Nafi name edit

--- src/App.vue
+++ src/App.vue
@@ -118,7 +118,7 @@ export default {
         },
         {
           "callsign": "Gallant",
-          "alias": "Ser Gilbert DuBois de Fouguex dit le Parravon Rallart XXVII",
+          "alias": "Ser Gilbert DuBois de Fouguex",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-Breton//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
